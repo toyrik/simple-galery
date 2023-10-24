6a5635fbd9cc4a9ee3450a26f96201a0972276d1
Toy Rik <toyrik@gmail.com>
2023-10-24 20:34:15 +0300

    Вынесение повторяющегося кода из отдельных вьюх

--- views/pages/gallery.php
+++ views/pages/gallery.php
@@ -1,15 +1,3 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="/assets/css/app.css">
-</head>
-<body>
+<?php include __DIR__ . '/../components/header.php'?>
 <h1>Gallery Page</h1>
-<script src="/assets/js/app.js"></script>
-</body>
-</html>
+<?php include __DIR__ . '/../components/footer.php'?>
