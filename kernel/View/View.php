09677c006cdc5835e3b6272c650910f9fd870919
Toy Rik <toyrik@gmail.com>
2023-10-24 20:57:23 +0300

    Ошибка для "потерянных" вьюх

--- kernel/View/View.php
+++ kernel/View/View.php
@@ -2,19 +2,35 @@
 
 namespace App\Kernel\View;
 
+use App\Kernel\Exceptions\ViewNotFoundException;
+
 class View
 {
     public function page(string $name)
     {
+        $viewPath = APP_PATH . '/views/pages/' . $name . '.php';
+
+        if (!file_exists($viewPath)) {
+            throw new ViewNotFoundException('View "' . $name . '" not found');
+        }
+
         extract([
             'view' => $this
         ]);
 
-        include_once APP_PATH . '/views/pages/' . $name . '.php';
+        include_once $viewPath;
     }
 
     public function component(string $name)
     {
-        include_once APP_PATH . '/views/components/' . $name . '.php';
+        $componentPath = APP_PATH . '/views/components/' . $name . '.php';
+
+
+        if (!file_exists($componentPath)) {
+            echo 'Component "' . $name . '" not found';
+            return;
+        }
+
+        include_once $componentPath;
     }
 }
